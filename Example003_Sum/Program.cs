a3b00f906b874e81cb36d182d1a41e6a991aa8c7
Sergey <syn@inbox.ru>
2023-08-08 12:09:09 +0500

    Example003_sum random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int namberA = 3;
-int namberB = 5;
+﻿int namberA = new Random().Next(1, 10);//1,2,3,4 ... 9
+//int namberA = 3;
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);
+//int namberB = 5;
+Console.WriteLine(namberB);
 int result = namberA + namberB;
 Console.WriteLine(result);
